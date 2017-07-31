bac688c6689698c4d99d55c2f813b005a91f82df
sujian <121116111@qq.com>
2017-07-31 22:25:27 +0800

    header description

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="header">
     <div class="content-wrapper">
+
       <div class="avatar">
-        <img src="" alt="">
+        <img :src="seller.avatar" height="64" width="64">
       </div>
+
+      <div class="content">
+
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+
+        <div v-if="seller.supports" class="support">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+
+      </div>
+
     </div>
 
     <div class="bulletin-wrapper"></div>
@@ -20,5 +40,32 @@
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixin.styl"
+  .header
+    color white
+    background black
+    .content-wrapper
+      padding 24px 12px 18px 24px
+      font-size 0px
+      .avatar
+        display inline-block
+      .content
+        display inline-block
+        font-size 14px
+        margin-left 16px
+        .title
+          margin 2px 0 8px 0
+          .brand
+            vertical-align top
+            display inline-block
+            width 32px
+            height 18px
+            bg-img("brand")
+            background-size 30px 18px
+            background-repeat no-repeat
+          .name
+            margin-left 6px
+            font-size 16px
+            line-height 18px
+            font-weight bold
 </style>
